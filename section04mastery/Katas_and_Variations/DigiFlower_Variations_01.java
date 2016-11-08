830553d3c4d71c2f062d33646351ef158143a97b
91078403 <91078403@mvusd.net>
2016-11-08 09:25:58 -0800

    digiFlower and Variations

--- section04mastery/Katas_and_Variations/DigiFlower_Variations_01.java
+++ section04mastery/Katas_and_Variations/DigiFlower_Variations_01.java
@@ -1,4 +1,4 @@
-package org.teachingkidsprogramming.section04mastery;
+package org.teachingkidsprogramming.section04mastery.Katas_and_Variations;
 
 import java.awt.Color;
 
@@ -6,23 +6,23 @@ import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
-public class DigiFlower
+public class DigiFlower_Variations_01
 {
   public static void main(String[] args)
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.Silver);
+    Tortoise.getBackgroundWindow().setBackground(PenColors.Blues.CornflowerBlue);
     Tortoise.setPenWidth(3);
     for (int i = 0; i < 15; i++)
     {
       Tortoise.turn(360 / 15);
       createColorPalette();
       //
-      drawOctagon();
+      drawOctogon();
     }
   }
-  private static void drawOctagon()
+  private static void drawOctogon()
   {
     for (int i = 0; i < 8; i++)
     {
@@ -33,10 +33,10 @@ public class DigiFlower
   }
   private static void createColorPalette()
   {
-    Color color1 = PenColors.Reds.Red;
-    Color color2 = PenColors.Oranges.DarkOrange;
-    Color color3 = PenColors.Yellows.Gold;
-    Color color4 = PenColors.Yellows.Yellow;
+    Color color1 = PenColors.Pinks.Fuchsia;
+    Color color2 = PenColors.Pinks.LightPink;
+    Color color3 = PenColors.Purples.DarkOrchid;
+    Color color4 = PenColors.Purples.Magenta;
     ColorWheel.addColor(color1);
     ColorWheel.addColor(color2);
     ColorWheel.addColor(color3);
